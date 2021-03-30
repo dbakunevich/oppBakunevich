69b0f1ce4db5ebd40633bd9808e22af62f7d79df
dmitry <d.bakunevich@g.nsu.ru>
2021-03-30 17:40:13 +0700

    123

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -1,4 +1,17 @@
 #include <iostream>
+#include "mpi.h"
+
+#define a 10e5
+#define e 10e-8
+
+#define DX 2
+#define DY 2
+#define DZ 2
+
+#define NX 1000
+#define NY 1000
+#define NZ 1000
+
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
