6d434987ea5d69fbcc00d3993a80a65be7ee491c
dmitry <d.bakunevich@g.nsu.ru>
2021-05-18 15:12:57 +0700

    first commit

--- lab5/main.cpp
+++ lab5/main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <pthread.h>
+#include <cmath>
+#include "mpi.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int size, rank;
+
+int startWeight;
+int startSize;
+int iterCount;
+
+int main(int argc, char *argv[]) {
+    int provided;
+    int error = MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    if (argc < 4) {
+        if (!rank) {
+            std::cout << "Usage: loadBalancing.exe iter_count list_size start_weight" << std::endl;
+        }
+        MPI_Finalize();
+        return 0;
+    }
+
+    iterCount = atoi(argv[1]);
+    startSize = atoi(argv[2]);
+    startWeight = atoi(argv[3]);
+
+    MPI_Finalize();
     return 0;
 }
